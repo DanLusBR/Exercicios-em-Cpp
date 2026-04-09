20250a31768c15a7a17fef7b255152692421086c
DanLusBR <samu.nico837@gmail.com>
2026-04-08 23:21:15 -0300

    Idades

---
+++ Idades/Idades.cpp
@@ -0,0 +1,39 @@
+// Problema "idades"
+// Fazer um programa para ler o nome e idade de duas pessoas. Ao final mostrar uma mensagem com os nomes e a idade média entre essas pessoas, com uma casa decimal, conforme exemplo. 
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <climits>
+
+using namespace std;
+
+int main() {
+
+    string nome1, nome2;
+    int idade1, idade2;
+    double media;
+
+    cout << "Dados da primeira pessoa:" << endl;
+    cout << "Nome: ";
+    getline(cin, nome1);
+    cout << "Idade: ";
+    cin >> idade1;
+
+    cin.ignore(INT_MAX, '\n');
+
+    cout << "Dados da segunda pessoa:" << endl;
+    cout << "Nome: ";
+    getline(cin, nome2);
+    cout << "Idade: ";
+    cin >> idade2;
+
+    media = (double)(idade1 + idade2) / 2.0;
+
+    cout << fixed << setprecision(1);
+    cout << "A idade media de " << nome1 << " e " << nome2 
+         << " eh de " << media << " anos." << endl;
+
+    return 0;
+
+}
